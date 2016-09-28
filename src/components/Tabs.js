5900ed030ab063d1a2a50c9a1ce4c8b02a394767
Stéphane Montlouis-Calixte <smontlouis@kilix.fr>
2016-09-28 22:18:15 +0200

    Install react native icons

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,65 +1,87 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import {
   View,
   Text,
   StyleSheet,
 } from 'react-native';
 
+import combineStyles from '../helpers/combineStyles';
+
+
 const styles = StyleSheet.create({
   tabs: {
     flexDirection: 'row',
     backgroundColor: 'white',
-    height: 60,
-  },
-  tabsActiveStyle: {
-    backgroundColor: '#B185FD',
+    height: 56,
   },
   tabLink: {
     flex: 1,
+  },
+  tabLinkActive: {
+    backgroundColor: 'blue',
+  },
+  tabContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#CECAFE',
+    backgroundColor: 'white',
   },
   tabLinkText: {
-    fontSize: 12,
+    fontSize: 11,
+    color: '#BFBFBF',
   },
 });
 
-const options = {
-  activeStyle: styles.tabsActiveStyle,
-  style: styles.tabLink,
-  underlayColor: 'transparent',
-};
+const links = [
+  {
+    to: '/',
+    icon: 'question-answer',
+    label: 'Topics',
+  },
+  {
+    to: '/favorites',
+    icon: 'favorite',
+    label: 'Favoris',
+  },
+  {
+    to: '/profile',
+    icon: 'account-circle',
+    label: 'Moi',
+  },
+  {
+    to: '/more',
+    icon: 'menu',
+    label: 'Plus',
+  }
+];
 
-const Tabs = () => (
-  <View style={styles.tabs}>
-    <Link
-      to="/"
-      {...options}
-    >
-      <Text style={styles.tabLinkText}>Topics</Text>
-    </Link>
-    <Link
-      to="/favorites"
-      {...options}
-    >
-      <Text style={styles.tabLinkText}>Favoris</Text>
-    </Link>
-    <Link
-      to="/profile"
-      {...options}
-    >
-      <Text style={styles.tabLinkText}>Moi</Text>
-    </Link>
-    <Link
-      to="/more"
-      {...options}
-    >
-      <Text style={styles.tabLinkText}>Plus</Text>
-    </Link>
-  </View>
-);
+const Tabs = () => {
+  const IconStyles = combineStyles({
+    tabLink: true,
+    tabLinkActive: false,
+  }, styles);
+  return (
+    <View style={styles.tabs}>
+      {links.map(({ to, icon, label }, i) =>
+        <Link
+          key={i}
+          to={to}
+          style={styles.tabLink}
+          underlayColor="transparent"
+          activeOpacity={0.5}
+        >
+          <View style={styles.tabContainer}>
+            <Icon name={icon} size={24} color="#BFBFBF" />
+            <Text style={styles.tabLinkText}>{label}</Text>
+          </View>
+        </Link>
+      )}
+    </View>
+  );
+};
 
 Tabs.PropTypes = {
 
