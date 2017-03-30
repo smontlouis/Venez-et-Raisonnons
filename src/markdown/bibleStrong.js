92fe1af85ce00c7f10d3cd7209ff17721572494d
Stéphane Montlouis-Calixte <smontlouis@kilix.fr>
2017-03-30 16:18:38 +0200

    Add bible strong and flow proptypes

--- src/markdown/bibleStrong.js
+++ src/markdown/bibleStrong.js
@@ -1,32 +1,12 @@
 const markdown =
-`La Bible est aujourd'hui le livre le plus traduit dans le monde. Chaque jour, des millions de personnes en quête de vérité tournent les pages de ce livre inspiré, pour y découvrir Dieu, apprendre à mieux le connaître, ou encore afin d'y trouver des réponses pertinentes à une vie pleine d'inquiétudes et de questionnements.
+`
+Les *numéros Strong* sont un système de renvoi par numérotation reliant les mots de la Bible aux mots hébreux et grecs des textes bibliques originaux.
 
-Les saintes Écritures sont une source intarissable de lumière et de vérité. En avoir une lecture linéaire et littérale ne suffit pas pour en comprendre le sens profond. Le véritable chercheur en quête de vérité devra creuser avec persévérance dans la Parole de Dieu par des études minutieuses et la prière afin d'affermir sa foi.
+On dit "Bible Strong" car les premières tentatives de numérotation de la Bible King James furent effectuées par James Strong au XIXe siècle. Son travail dura 35 ans !
 
-Les outils efficaces d'étude de la Bible se sont développés à travers les siècles mais le monde francophone demeure encore pauvre en la matière. L'étude approfondie des Textes sacrés ne semble aujourd'hui réservée qu'à une certaine élite, capable de suivre des cursus théologiques ou encore d'apprendre des langues anciennes ou étrangères.
+Le besoin de corriger la Strong s'est rapidement fait sentir lorsqu'il a fallu la reporter sur d'autres traductions de la Bible. Certains auteurs ont du créer leur propre système de numérotation. C'est le cas de Jack Caucheranne qui a renuméroté les mots grecs et hébreux pour la Bible Second.
 
-Est-ce une fatalité ?
-
-## Qui-sommes-nous ?
-
-Nous sommes un groupe de jeunes chrétiens persuadés que Dieu s'adresse par sa parole à toute âme sincère qui désire le connaître et que l'étude de la Bible n'est pas une question d'élitisme intellectuel. Nous souhaitons aider à la propagation de la Parole de Dieu dans le milieu francophone ainsi qu'à favoriser son étude.
-
-## Le projet
-
-Le projet "Venez et Raisonnons" a pour objectif la mise à disposition d'outils efficaces d'étude de la Bible pour tous ceux qui souhaitent développer et affermir une foi réfléchie en Dieu par sa Parole.
-
-Pour ce faire, nous développons une application mobile iOS/Android/Windows Phone/Web contenant :
-
-Des études bibliques claires et concises traitant de sujets divers et variés.
-Des questions fréquemment posées avec les réponses que la Parole de Dieu y apporte
-La possibilité de nous poser vous mêmes vos questions et d'en recevoir une réponse claire basée sur la Bible
-Différentes versions de la Bible
-Un dictionnaire biblique
-Une version de la Bible contenant les STRONG* hébreux et grec
-
-Ainsi que d'autres supports et outils d'études à venir par la suite.
-
-Nous sommes persuadés que tout cela sera d’une grande aide pour le monde francophone chrétien mais pour mener à terme un tel projet nous avons aussi besoin de votre aide. C’est la raison pour laquelle nous vous demandons de prendre part vous aussi à cette initiative en nous soutenant pour vos dons.
+Donc normalement, l'on devrait dire "Bible Caucheranne" pour le monde français, mais par abus de langage, on utilise toujours Strong.
 `
 
 export default markdown
