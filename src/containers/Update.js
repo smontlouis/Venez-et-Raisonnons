b7e22110b0b7ff004f1f395389b5605e9c8ad4c1
Stéphane Montlouis-Calixte <smontlouis@kilix.fr>
2017-09-15 13:10:26 +0200

    Fix typo

--- src/containers/Update.js
+++ src/containers/Update.js
@@ -16,7 +16,7 @@ const converter = new ShowDown.Converter()
 const markdownToHtml = converter.makeHtml(
 `Une nouvelle mise à jour contenant beaucoup de nouveautés arrive fin-septembre !
 
-Afin de ne pas perdre vos données lors de la mise à jour, il est fortement recommander de sauvegarder vos données en vous créant un compte dans Plus -> Connexion.
+Afin de ne pas perdre vos données lors de la mise à jour, il est fortement recommandé de sauvegarder vos données en vous créant un compte dans Plus -> Connexion.
 
 Une fois connecté, votre activité sera automatiquement synchronisée dans le cloud. Vous pourrez ensuite retrouver vos questions lues / favories / likées en vous connectant avec votre compte !
 
